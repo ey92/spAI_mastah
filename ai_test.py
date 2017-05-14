7ddb8baa13d5beb92384386fb1ab72768dfe63c7
Kairi700 <Kairi700@gmail.com>
2017-05-14 03:23:36 -0400

    Modified test file

--- ai_test.py
+++ ai_test.py
@@ -5,6 +5,15 @@ import cPickle as pickle
 
 DICTIONARY_FILE = "words.txt"
 word_list = []
+word_grid = {}
+assasian_word = ""
+blue_words = 9
+red_words = 8
+civilian_num = 7
+BLUE_TEAM = 0
+RED_TEAM = 1
+CIVILIAN = 2
+ASSASIAN = 3
 word_guessed = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 word_guessed2 = [0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0]
 total_words = 25
@@ -29,6 +38,53 @@ def getWords():
 			#Don't add if already there
 			if word_list.count(chosen_word)==0:
 				word_list.append(chosen_word)
+
+def createWordGrid():
+	# global word_list
+	global word_grid
+	global word_list
+	global assasian_word
+	##Get random set of words in a dictionary
+	#Assigned here incase of reset
+	word_grid = {} #GLOBAL
+	word_list = [] #GLOBAL, resets for every game
+
+	#Populates word_list
+	# word_list = getWords()	
+	getWords()
+
+	##Assign key as word w/ value as BLUE_TEAM, RED_TEAM, CIVILIAN, or ASSASIAN
+	assasian_num = 1
+	civilian_num = total_words-blue_words-red_words-assasian_num
+
+	#Insert blue into assigning order
+	assign_order = [BLUE_TEAM]*blue_words
+
+	#Insert red in assigning order
+	for i in range(0,red_words):
+		assign_num = random.randint(0,len(assign_order)-1)
+		assign_order.insert(assign_num,RED_TEAM)
+
+	#Insert civilian in assigning order
+	for i in range(0,civilian_num):
+		assign_num = random.randint(0,len(assign_order)-1)
+		assign_order.insert(assign_num,CIVILIAN)
+
+	#Insert assasian in assigning order
+	assign_num = random.randint(0,len(assign_order)-1)
+	assign_order.insert(assign_num,ASSASIAN)
+
+	# print("Burp")
+	# print(assign_order)
+	# print(word_list)
+
+	#Assign words a label
+	for i in range(0,len(word_list)):
+		word = word_list[i]
+		assign_num = assign_order[i]
+		# print("Word: "+word+", ID:  "+str(assign_num))
+		word_grid.update({word:assign_num})	
+		if assign_num==ASSASIAN: assasian_word = word
 
 
 if __name__ == "__main__":
