e37ad033c79969f0cd0ee408e013e68588e51358
Kairi700 <Kairi700@gmail.com>
2017-05-13 22:13:10 -0400

    Indents fixed?

--- codenames_ai.py
+++ codenames_ai.py
@@ -17,64 +17,64 @@ class spyPlayer():
 		self.team_num = team_num
 		self.sim_matrix = self.generateSimMatrix()
 		self.rel_pool = self.generateRelRocchio()
-		self.irrel_pool = self.generateIrrelRocchio()    
+		self.irrel_pool = self.generateIrrelRocchio()
 		self.idx_to_bankword = self.generateBankWordMap()
 		self.bankword_to_idx = self.generateInvertedBankWord()
 		self.idx_to_codeword = self.generateCodeWordMap()
 		self.codeword_to_idx = self.generateInvertedCodeWord()
 
-    def generateSimMatrix():
-    	"""Returns a numpy array mapping Codename words to other possible words"""
-    	sim_mat = np.empty([400,6459])
-			with open(SIM_PICKLE_HEAD+str("1")+".pickle",'rb') as f:
-        sim_mat = pickle.load(f)
+	def generateSimMatrix():
+		"""Returns a numpy array mapping Codename words to other possible words"""
+		sim_mat = np.empty([400,6459])
+		with open(SIM_PICKLE_HEAD+str("1")+".pickle",'rb') as f:
+			sim_mat = pickle.load(f)
 
-     	return sim_mat
-    	
-    	#Should trim it such that only contains the words pertaining to the game?
-    def generateRelRocchio():
-    	rel_dict = {}
-			with open(RELEVANT_PICKLE,'rb') as f:
-        rel_dict = pickle.load(f)
+		return sim_mat
+		
+		#Should trim it such that only contains the words pertaining to the game?
+	def generateRelRocchio():
+		rel_dict = {}
+		with open(RELEVANT_PICKLE,'rb') as f:
+			rel_dict = pickle.load(f)
 
-     	return rel_dict
+		return rel_dict
 
-    def generateIrrelRocchio():
-    	irrel_dict = {}
-			with open(IRRELEVANT_PICKLE,'rb') as f:
-        irrel_dict = pickle.load(f)
+	def generateIrrelRocchio():
+		irrel_dict = {}
+		with open(IRRELEVANT_PICKLE,'rb') as f:
+			irrel_dict = pickle.load(f)
 
-     	return irrel_dict
+		return irrel_dict
 
-    def generateBankWordMap():
-    	wordmap = []
+	def generateBankWordMap():
+		wordmap = []
 		with open(BANKWORDS_PICKLE,'rb') as f:
-        	wordmap = pickle.load(f)
+			wordmap = pickle.load(f)
 
-      	return wordmap
+		return wordmap
 
-    def generateInvertedBankWord():
-    	wordmap = {}
+	def generateInvertedBankWord():
+		wordmap = {}
 		with open(INVERTED_BANKWORDS_PICKLE,'rb') as f:
-        	wordmap = pickle.load(f)
+			wordmap = pickle.load(f)
 
-     	return wordmap
+		return wordmap
 
-    def generateCodeWordMap():
+	def generateCodeWordMap():
 		wordmap = []
 		with open(CODEWORDS_PICKLE,'rb') as f:
-        	wordmap = pickle.load(f)
+			wordmap = pickle.load(f)
 
-      	return wordmap
+		return wordmap
 
 	def generateInvertedCodeWord():
 		wordmap = {}
 		with open(INVERTED_CODEWORDS_PICKLE,'rb') as f:
-    		wordmap = pickle.load(f)
+			wordmap = pickle.load(f)
 
- 		return wordmap
+		return wordmap
 
-    def getRelevantEntry(word):
+	def getRelevantEntry(word):
 		return self.rel_pool.get(word,[])
 
 	def getIrrelevantEntry(word):
@@ -105,7 +105,7 @@ class spyPlayer():
 			original_rel.update({entry:original_list})
 
 		with open(RELEVANT_PICKLE,'wb') as f:
-    		pickle.dump(original_rel,f)
+			pickle.dump(original_rel,f)
 
 
 		#Update original irrelevant rocchio with only NEW entries made during the game 
@@ -119,15 +119,15 @@ class spyPlayer():
 			original_irrel.update({entry:original_list})
 
 		with open(IRRELEVANT_PICKLE,'wb') as f:
-    		pickle.dump(original_irrel,f)
+			pickle.dump(original_irrel,f)
 
 
 class spyMaster(spyPlayer):
 
 	def interpretGameboard(word_grid):
 		"""Returns a 4-tuple of the words REMAINING for the spy master's team, 
-		   the opposing team, the civilians, and the assasin (represented by boom)
-		   Note: Already guessed words are noted by a negative sign"""
+		 the opposing team, the civilians, and the assasin (represented by boom)
+		 Note: Already guessed words are noted by a negative sign"""
 		team_words = []
 		opp_words = []
 		civ_words = []
@@ -147,20 +147,20 @@ class spyMaster(spyPlayer):
 
 		return team_words, opp_words, civ_words, boom_word
 
- 	def createClue(word_list, word_grid):
-	  	"""Creates a clue based off the state of the gameboard [word_grid]
-	  		word_grid: dictionary containing the "identity of each word"
-	  	"""
-	  	clue = ""
-	  	clue_num = 1
+	def createClue(word_list, word_grid):
+		"""Creates a clue based off the state of the gameboard [word_grid]
+			word_grid: dictionary containing the "identity of each word"
+		"""
+		clue = ""
+		clue_num = 1
 
-	  	team_words, opp_words, civ_words, boom_word = self.interpretGameboard(word_grid)
+		team_words, opp_words, civ_words, boom_word = self.interpretGameboard(word_grid)
 
-	  	#Find words most relevant to each other
+		#Find words most relevant to each other
 
-	  	#Rocchio away the synonyms of the opp_words, civ_words, and boom_word
-	  	
-	  	return clue, clue_num
+		#Rocchio away the synonyms of the opp_words, civ_words, and boom_word
+		
+		return clue, clue_num
 
 
 class spyAgent(spyPlayer):
@@ -176,35 +176,35 @@ class spyAgent(spyPlayer):
 		
 		return not_guessed
 
-  	# NOTE: returns a LIST of guesses, must be iterated through in order to process in main game
-  	def makeGuesses(word_list, guessed,clue,num_words):
-	  	guesses = []
+	# NOTE: returns a LIST of guesses, must be iterated through in order to process in main game
+	def makeGuesses(word_list, guessed,clue,num_words):
+		guesses = []
 
-	  	# USE INSTEAD OF WORD_LIST
-	  	word_choices = self.getNonGuessed(word_list, guessed)
+		# USE INSTEAD OF WORD_LIST
+		word_choices = self.getNonGuessed(word_list, guessed)
 
-	  	# Get sim vector for the clue
-	  	clue_idx = self.bankword_to_idx.get(clue)
-	  	clue_vec = self.sim_matrix[clue_idx]
+		# Get sim vector for the clue
+		clue_idx = self.bankword_to_idx.get(clue)
+		clue_vec = self.sim_matrix[clue_idx]
 
-	  	# Rocchio with sim vectors for synonyms and antonyms of the clue
-	  	mod_clue_vec = clue_vec # TODO Rocchio
+		# Rocchio with sim vectors for synonyms and antonyms of the clue
+		mod_clue_vec = clue_vec # TODO Rocchio
 
-	  	# Get argsort of resulting vector as ranking
-	  	ranking = np.argsort(mod_clue_vec)
+		# Get argsort of resulting vector as ranking
+		ranking = np.argsort(mod_clue_vec)
 
-	  	counter = 0
-	  	while len(guesses)<num_words:
-	  		code_check = ranking[counter]
-	  		codeword = idx_to_codeword[code_check] 
-	  		if word_choices.count(codeword) ==1:guesses.append(codeword)
-	  		counter+=1
+		counter = 0
+		while len(guesses)<num_words:
+			code_check = ranking[counter]
+			codeword = idx_to_codeword[code_check] 
+			if word_choices.count(codeword) ==1:guesses.append(codeword)
+			counter+=1
 
-	  	# While len(guesses) < num_words
-		  	#Iterate through ranking via counter
-		  	#if idx_to_codeword[counter] is in word_list
-		  		#Append result to guesses
+		# While len(guesses) < num_words
+			#Iterate through ranking via counter
+			#if idx_to_codeword[counter] is in word_list
+				#Append result to guesses
 
-  		return guesses
+		return guesses
 
 
